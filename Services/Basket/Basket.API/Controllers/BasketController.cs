e964c22b04343d696b9d3f684b3f8d014cc158d1
ThembaFumani <themba.maluleke@nolosol.com>
2024-09-19 09:16:50 +0200

    pushing to main

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
+
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+        }
+
+        [HttpGet("{username}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var basket = await _repository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            foreach(var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            await _repository.DeleteBasket(username);
+            return Ok();
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+	public class GetOrderListQuery : IRequest<List<OrdersVm>>
+	{
+        public string  UserName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
+
